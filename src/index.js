30423b783d063e6fb9fdf024750a9c2b21555b34
Anton Kamenetskiy <lemaxone@gmail.com>
2020-10-19 18:34:10 +0300

    feat: create reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 module.exports = function reverse (n) {
-  
+    let new_n = n.toString().match(/[0-9]/g);
+    let resultStr = '';
+    for (let i = new_n.length - 1; i >=0 ; i--) {
+        resultStr += new_n[i];
+    }
+    let resultNum = Number(resultStr);
+    return resultNum;
 }
